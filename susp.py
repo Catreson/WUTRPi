527906d6c7200f019ae5a41edd78ae052b12245e
Catreson <arekdagiel@gmail.com>
2023-09-23 18:59:03 +0200

    Update susp.py

--- susp.py
+++ susp.py
@@ -3,7 +3,6 @@ import time
 import logging
 from lib import ADS1263
 from common import MQTT_CLIENT, SHM, READ_TRIGGER
-from numba import njit, jit
 
 class SUSPENSION():
     ANALOG_RANGE = 0x7fffffff
@@ -68,41 +67,30 @@ class SUSPENSION():
                 dat = line.split(',')
                 self.corr_dict[dat[0]] = float(dat[1])
 
-    @staticmethod
-    @njit
-    def ch_shock(potentiometer_length):
+    def ch_shock(self, potentiometer_length):
         return 0.0030 * pow(potentiometer_length, 2) - 1.6297 * potentiometer_length + 105.3946
-
-    @staticmethod
-    @njit
-    def ch_steer(potentiometer_length):
+    def ch_steer(self, potentiometer_length):
         return -0.0006 * pow(potentiometer_length, 2) - 0.4231 * potentiometer_length + -0.0026
 
-    @staticmethod
-    @njit
-    def potentiometer(analog_value, potentiometer_length):
-        return potentiometer_length * analog_value / 0x7fffffff
-
+    def potentiometer(self, analog_value, potentiometer_length):
+        return potentiometer_length * analog_value / self.ANALOG_RANGE
 
     def read_data(self):
         ptim = time.time()
         self.val = self.ADC.ADS1263_GetAll(self.channelList)
-        print(f'ADC {time.time() - ptim}')
         susp_f = self.potentiometer(analog_value=(self.val[0] - self.corr_dict['susp_f']), potentiometer_length=150)
         pot_r = self.potentiometer(analog_value=(self.val[1] - self.corr_dict['susp_r']), potentiometer_length=75)
         susp_r = self.ch_shock(pot_r)
         p_brake = self.potentiometer(analog_value=self.val[2] - self.corr_dict['p_brake'], potentiometer_length=227)
         pot_sa = self.potentiometer(analog_value=self.val[3] - self.corr_dict['steer_angle'], potentiometer_length=150)
         steer_angle = self.ch_steer(pot_sa)
-        print(f'calc {time.time() - ptim}')
         self.cm.save('susp_f', susp_f)
         self.cm.save('susp_r', susp_r)
         self.cm.save('p_brake', p_brake)
         self.cm.save('steer_angle', steer_angle)
-        print(f'shm {time.time() - ptim}')
         even = f'susp,{time.time() - self.mqtit.timestam},{susp_f} {susp_r} {p_brake} {steer_angle},bike/sensor/susp,string'
         self.mqtit.send(topic=self.write_topic, event=even)
-        print(f'mqtt {time.time() - ptim}')
+        print(time.time() - ptim)
 
     def __del__(self):
         print("Program end")
